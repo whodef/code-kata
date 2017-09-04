deb36b9d3a6d190563c0291f39883ff82fa13f03
whodef <tatyana@selyuk.com>
2017-09-04 18:22:50 +1000

    added new solve

---
+++ kantor-learn-js/сhecking-the-standard.js
@@ -0,0 +1,9 @@
+
+// Проверка логина
+let offName = prompt('Каково \"официальное\" название JavaScript?', '');
+
+    if (offName === 'EcmaScript') {
+        alert('Верно!');
+    } else {
+        alert('Не знаете? «ECMAScript»!');
+    }
